bd3650ac6b97a5a3590ea067b0f93886e7386fdb
Atul R <atulanand94@gmail.com>
2017-12-02 14:45:58 +0530

    Added 2.6.0 and removed backward compatible code and added warning to console.log

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const {ReactNativeExceptionHandler} = NativeModules;
 const noop = () => {};
 
 export const setJSExceptionHandler = (customHandler = noop, allowedInDevMode = false) => {
+  if ((typeof allowedInDevMode !== 'boolean') || (typeof customHandler !== 'function')) {
+    console.log('setJSExceptionHandler is called with wrong argument types.. first argument should be callback function and second argument is optional should be a boolean');
+    console.log('Not setting the JS handler .. please fix setJSExceptionHandler call');
+    return;
+  }
   const allowed = allowedInDevMode ? true : !__DEV__;
   if (allowed) {
     global.ErrorUtils.setGlobalHandler(customHandler);
@@ -17,24 +22,12 @@ export const setJSExceptionHandler = (customHandler = noop, allowedInDevMode = f
 
 export const getJSExceptionHandler = () => global.ErrorUtils.getGlobalHandler();
 
-export const setNativeExceptionHandler = (forceApplicationToQuit = true, customErrorHandler = noop) => {
-  var handler = customErrorHandler;
-
-  // backward compatible API
-  if (typeof forceApplicationToQuit === 'function') {
-    handler = forceApplicationToQuit;
+export const setNativeExceptionHandler = (customErrorHandler = noop, forceApplicationToQuit = true) => {
+  if ((typeof customErrorHandler !== 'function') || (typeof forceApplicationToQuit !== 'boolean')) {
+    console.log('setNativeExceptionHandler is called with wrong argument types.. first argument should be callback function and second argument is optional should be a boolean');
+    console.log('Not setting the native handler .. please fix setNativeExceptionHandler call');
+    return;
   }
-
-  if (typeof customErrorHandler === 'boolean') {
-    forceApplicationToQuit = customErrorHandler;
-  }
-
-  customErrorHandler = handler;
-
-  if (typeof customErrorHandler !== 'function') {
-    customErrorHandler = noop;
-  }
-  
   if (Platform.OS === 'ios') {
     ReactNativeExceptionHandler.setHandlerforNativeException(customErrorHandler);
   } else {
