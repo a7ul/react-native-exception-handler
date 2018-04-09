355c5444d721ed0ed3c4fb5ca8286c579c79db69
TomMahle <tommahle@gmail.com>
2018-04-09 12:20:01 -0500

    Change import priority of RCTBridgeModule
    This appears to be the accepted fix for this issue: https://github.com/master-atul/react-native-exception-handler/issues/46 per https://github.com/facebook/react-native/issues/15775#issuecomment-326930316 and other posts in the same thread.

--- ios/ReactNativeExceptionHandler.h
+++ ios/ReactNativeExceptionHandler.h
@@ -1,8 +1,8 @@
 
-#if __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
-#else
+#if __has_include(<React/RCTBridgeModule.h>)
 #import <React/RCTBridgeModule.h>
+#else
+#import "RCTBridgeModule.h"
 #endif
 
 #import  <UIKit/UIKit.h>
