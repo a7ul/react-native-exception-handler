d14825993f8a1a5041852f9681f49d37bd3e4412
Robrecht Meersman <robrechtmeersman@hotmail.com>
2021-03-23 17:51:13 +0100

    fix: still call console.error when hoisting it (#149)
    Co-authored-by: Robrecht Meersman <robrecht.meersman@inthepocket.mobi>

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { ReactNativeExceptionHandler } = NativeModules;
 const noop = () => { };
 
 export const setJSExceptionHandler = (customHandler = noop, allowedInDevMode = false) => {
-  if (typeof allowedInDevMode !== "boolean" || typeof customHandler !== "function"){
+  if (typeof allowedInDevMode !== "boolean" || typeof customHandler !== "function") {
     console.log("setJSExceptionHandler is called with wrong argument types.. first argument should be callback function and second argument is optional should be a boolean");
     console.log("Not setting the JS handler .. please fix setJSExceptionHandler call");
     return;
@@ -13,7 +13,11 @@ export const setJSExceptionHandler = (customHandler = noop, allowedInDevMode = f
   const allowed = allowedInDevMode ? true : !__DEV__;
   if (allowed) {
     global.ErrorUtils.setGlobalHandler(customHandler);
-    console.error = (message, error) => global.ErrorUtils.reportError(error); // sending console.error so that it can be caught
+    const consoleError = console.error;
+    console.error = (...args) => {
+      global.ErrorUtils.reportError(...args);
+      consoleError(...args);
+    };
   } else {
     console.log("Skipping setJSExceptionHandler: Reason: In DEV mode and allowedInDevMode = false");
   }
