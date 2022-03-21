a1cf16140dd112f581f66ae4a044d99dcfc9fdfe
KukuJerung <khairulfilhan94@gmail.com>
2022-03-21 16:44:27 +0800

    update typing index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,17 @@
-export type JSExceptionHandler = (error: Error, isFatal: boolean) => void;
-export type NativeExceptionHandler = (exceptionMsg: string) => void;
+// export type JSExceptionHandler = (error: Error, isFatal: boolean) => void;
+// export type NativeExceptionHandler = (exceptionMsg: string) => void;
 
-declare const getJSExceptionHandler: () => JSExceptionHandler;
+// declare const getJSExceptionHandler: () => JSExceptionHandler;
 
-declare const setJSExceptionHandler: (handler: JSExceptionHandler, allowInDevMode?: boolean) => void;
+// declare const setJSExceptionHandler: (
+//   handler: JSExceptionHandler,
+//   allowInDevMode?: boolean
+// ) => void;
 
-declare const setNativeExceptionHandler: (
-	handler: NativeExceptionHandler,
-	forceAppQuit?: boolean, // Android only
-	executeDefaultHandler?: boolean,
-) => void;
+// declare const setNativeExceptionHandler: (
+//   handler: NativeExceptionHandler,
+//   forceAppQuit?: boolean, // Android only
+//   executeDefaultHandler?: boolean
+// ) => void;
+
+declare const init: (logApiUrl: string) => void;
